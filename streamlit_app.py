65160bf511a584b091ff6820e58e22dad59f144d
kapitanTiyago <103943016+kapitanTiyago@users.noreply.github.com>
2025-12-12 23:05:20 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,9 @@ st.write('The name on your Smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:', my_dataframe
@@ -30,12 +31,6 @@ if ingredients_list:
         st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
-    #st.write(ingredients_string)
-    #my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-    #        values ('""" + ingredients_string + """','""" +name_on_order+ """')"""
-
-    st.write(my_insert_stmt)
 
     time_to_insert = st.button('Submit Order')
     
